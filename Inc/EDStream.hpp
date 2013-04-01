8c35d007b5888c42cd46bbfe49b4d706ccf07170
Nicholas "LB" Braden <LB@LB-Stuff.com>
2013-03-31 20:52:14 -0500

    Add EDOStream
    EDIStream and EDOStream are both still untested!

--- Inc/EDStream.hpp
+++ Inc/EDStream.hpp
@@ -25,7 +25,7 @@ struct EDIStream : std::basic_istream<TCHAR>
 
 	private:
 		std::size_t pb;
-		std::vector<TCHAR> b;
+		std::vector<char_type> b;
 
 		virtual int_type underflow()
 		{
@@ -54,11 +54,58 @@ private:
 	EDIStream &operator=(EDIStream const&);
 };
 
-//struct EDOStream : std::basic_ostream<TCHAR>
-//{
-//	struct Buf : std::basic_streambuf<TCHAR>
-//	{
-//	};
-//};
+struct EDOStream : std::basic_ostream<TCHAR>
+{
+	struct Buf : std::basic_streambuf<TCHAR>
+	{
+		Buf(mv *mV, SerializedED *&SED) : mV(mV), SED(SED)
+		{
+			setp(&b.front(), &b.front());
+		}
+
+	private:
+		mv *mV;
+		SerializedED *&SED;
+		std::vector<char_type> b;
+
+		virtual int_type overflow(int_type ch)
+		{
+			if(ch != traits_type::eof())
+			{
+				b.push_back(char_type(ch));
+				setp(&b.front(), &b.back()+1, &b.back()+1);
+			}
+			return traits_type::eof();
+		}
+		virtual int sync()
+		{
+			std::size_t size = (&b.back() + 1) - &b.front();
+			SerializedED *t = (SerializedED *)mvReAllocEditData(mV, SED, sizeof(SerializedED)+size);
+			if(t)
+			{
+				SED = t;
+				memcpy(&SED->data, &b.front(), size);
+				return 0;
+			}
+			return -1;
+		}
+
+		Buf(Buf const&);
+		Buf &operator=(Buf const&);
+	};
+
+	EDOStream(mv *mV, SerializedED *&SED) : b(mV, SED), std::basic_ostream<TCHAR>(&b)
+	{
+	}
+	virtual ~EDOStream()
+	{
+	}
+
+private:
+	Buf b;
+
+	EDOStream(EDOStream const&);
+	EDOStream &operator=(EDOStream const&);
+};
 
 #endif
